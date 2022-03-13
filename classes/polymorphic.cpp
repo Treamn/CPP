dcc0081e362d0d1d096f72bf99e1eb07f9fa27f0
whyy <897862595@qq.com>
2022-03-13 16:52:39 +0800

    to achive polymorphic need add 'virtual' before define function

---
+++ classes/polymorphic.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+using namespace std;
+
+class animal{
+    protected:
+        int feet;
+    public:
+        animal(int f):feet(f){}
+        virtual int get(){
+            cout << "it has " << feet << "feets." << endl;
+            return 0;
+        }
+};
+
+class dog: public animal{
+    public:
+        dog(int f):animal(f){}
+        int get(){
+            cout << "dog has " << feet << " feets." << endl;
+            return 0;
+        }
+};
+
+class swan: public animal{
+    protected:
+        int swing;
+    public:
+        swan(int s, int f):animal(f),swing(s){}
+        int get(){
+            cout << "swan has " << feet << " feets ";
+            cout << "and " << swing << " swings" << endl;
+            return 0;
+        }
+};
+
+
+
+int main(void){
+    animal *cute;
+    dog wangcai(4);
+    swan se(2,2);
+    cute = &wangcai;
+    cute->get();
+
+    cute = &se;
+    cute->get();
+    return 0;
+}
