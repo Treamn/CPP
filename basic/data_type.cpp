53211aa564c4c8f3856ceb63aa594a6fdeb39267
whyy <897862595@qq.com>
2022-03-13 10:44:37 +0800

    test string

--- basic/data_type.cpp
+++ basic/data_type.cpp
@@ -6,6 +6,11 @@ int main()
 {
     float a = 12.0;
     cout<<typeid(a).name()<<endl;
-    cout<<a;
+    cout<<a<<endl;
+
+    string s = "This is a test text!";
+    cout<<typeid(s).name()<<endl;
+    cout<<s<<endl;
+
     return 0;
 }
