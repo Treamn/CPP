ce05dc1f00fcf991f00bb0ad970e92ee586656da
whyy <897862595@qq.com>
2022-03-15 19:30:09 +0800

    人家的代码写的太牛啦,自己cout临摹一下

--- leetcode/2044.cpp
+++ leetcode/2044.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <bitset>
 using namespace std;
 
 class Solution {
@@ -13,9 +14,22 @@ public:
             int cur = 0;
             for(int j = 0; j < nums.size(); j++){
                 if(i & (1 << j)){
-                    cur |= nums[j];
+                    // cur |= nums[j];
+                    bitset<8>jb(1 << j);
+                    bitset<8>ib(i);
+                    int number = (i & (1 << j));
+                    bitset<8>bs(number);
+                    // cout << j << "  " << i << "  " << number << endl;
+                    // cout << jb << "   " << ib << "  " << bs ;
+                    cout << i << "  " << j << " ";
+                    // cout << bs;
                 }
+                // else{
+                //     cout << "this is zero" << endl;;
+                // }
+                // cout << endl;
             }
+            cout << endl;
             if(cur == max){
                 sum++;
             }
@@ -27,6 +41,8 @@ public:
 
 int main(){
     Solution ans;
-    vector<int> nums = {2,2,2};
-    cout << ans.countMaxOrSubsets(nums);
+    vector<int> nums = {3,2,1,5};
+    // cout << ans.countMaxOrSubsets(nums);
+    ans.countMaxOrSubsets(nums);
 }
+    
