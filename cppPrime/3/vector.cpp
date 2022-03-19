278a52a8d9d81959b5e52b8db7da445f31c01c75
whyy <897862595@qq.com>
2022-03-19 19:28:52 +0800

    无事发生

--- cppPrime/3/vector.cpp
+++ cppPrime/3/vector.cpp
@@ -3,10 +3,18 @@
 using namespace std;
 
 int main(){
-    vector<int> vec;
-    for(int i = 0 ; i != 100; i++)
-        vec.push_back(i);
+//     vector<int> vec;
+//     for(int i = 0 ; i != 100; i++)
+//         vec.push_back(i);
 
-    for(int i = 0; i != vec.size(); i++)
-        cout << vec[i] << ' ';
+//     for(int i = 0; i != vec.size(); i++)
+//         cout << vec[i] << ' ';
+    vector<unsigned> scores(11, 0);
+    unsigned grade;
+    while (cin >> grade)
+    {
+        if(grade <= 100)
+            ++scores[grade/10];
+    }
+    
 }
