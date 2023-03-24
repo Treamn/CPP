84951bf0b0a6dac172f9cd640256ee78430b1d3b
YuJiang <897862595@qq.com>
2023-03-24 10:08:43 +0800

    思路没错,但是超时了

--- leetcode/121/121.cpp
+++ leetcode/121/121.cpp
@@ -5,10 +5,21 @@ using namespace std;
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
-        vector<int> sub_set;
+        int mp = 0;
+
+        for(int i = 0; i < prices.size()-1; i++){
+            for(int j = i+1; j < prices.size(); j++){
+                int temp = prices[j] - prices[i];
+                // cout << i << " " << j << " " << temp << endl;
+                mp = max(mp, temp);
+            }
+        }
+        return mp;
     }
 };
 
 int main(){
-    
+    vector<int> vec{7,6,4,3,1};
+    Solution s1;
+    cout << s1.maxProfit(vec);
 }
