90b55a7f281f0162244743b7d29230ca0873f4b2
YuJiang <897862595@qq.com>
2023-03-27 10:25:44 +0800

    print all substr

--- leetcode/1638/1638.cpp
+++ leetcode/1638/1638.cpp
@@ -7,9 +7,9 @@ class Solution {
 public:
     int countSubstrings(string s, string t) {
         int ans = 0;
-        if(s.size() == 1 || t.size() == 1){
-            return ans;
-        }
+        // if(s.size() == 1 || t.size() == 1){
+        //     return ans;
+        // }
 
         // for(int i = 0; i < s.size(); i++){
         //     for(int j = 0; j < t.size(); j++){
@@ -19,28 +19,30 @@ public:
         //     }
         // }
 
-        for(int i = 1 ; i <= s.size(); i++){
-            for(int j = 0; j + i < s.size(); j++){
-                string subj = s.substr(j,i);
-                for(int k = 0; k + i < t.size(); k++){
-                    string subk = t.substr(k,1);    
+        // for(int i = 1 ; i <= s.size(); i++){
+        //     for(int j = 0; j + i < s.size(); j++){
+        //         string subj = s.substr(j,i);
+        //         for(int k = 0; k + i < t.size(); k++){
+        //             string subk = t.substr(k,1);    
 
-                    int status = 0;
-                    for(int x = 0; x < i; x++){
-                        if(subj[x] != subk[x]){
-                            status++;
-                        }
-                    }
-                    if(status == 1){
-                        ans++;
-                    }   
-                    status = 0;
-                }
+        //             int status = 0;
+        //             for(int x = 0; x < i; x++){
+        //                 if(subj[x] != subk[x]){
+        //                     status++;
+        //                 }
+        //             }
+        //             if(status == 1){
+        //                 ans++;
+        //             }   
+        //             status = 0;
+        //         }
+        //     }
+        // }
+        for(int i = 0; i< s.size(); i++){
+            for(int k = 0; i+k <= s.size(); k++){
+                cout << s.substr(i, k) << endl;
             }
         }
-
-
-
 
 
         return ans;
@@ -50,9 +52,9 @@ public:
 
 
 int main(){
-    string s{"ab"};
+    string s{"abcde"};
     string t{"bb"};
     // cout << s.substr(0,2);
     Solution s1;
-    cout << s1.countSubstrings(s,t);
+    s1.countSubstrings(s,t);
 }
