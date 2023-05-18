7965f1784eb72ce2d04dbe74e5452b9a18bd9c7e
YuJiang <897862595@qq.com>
2023-05-18 14:54:51 +0800

    test

--- test.cpp
+++ test.cpp
@@ -1,80 +1,89 @@
-#include <iostream>
-#include <vector>
+// #include <iostream>
+// #include <vector>
 
-using namespace std;
+// using namespace std;
 
-class Solution {
-public:
-    int minOperationsMaxProfit(vector<int>& customers, int boardingCost, int runningCost) {
-        int n = customers.size();
-        vector<int> line;
-        int ans = 0;
-        // int wait = 0;
-        // int lun = 0;
+// class Solution {
+// public:
+//     int minOperationsMaxProfit(vector<int>& customers, int boardingCost, int runningCost) {
+//         int n = customers.size();
+//         vector<int> line;
+//         int ans = 0;
+//         // int wait = 0;
+//         // int lun = 0;
 
-        for(int i = 0; i < n; i++){
-            if(i != (n - 1)){
-                if(customers[i] <= 4){
-                    line.push_back(customers[i]);
-                }else{
-                    int temp = customers[i] / 4;
-                    for(int j = 0; j < temp; j++){
-                        line.push_back(4);
-                    }
-                    customers[i + 1] += customers[i] % 4;
-                }
-            }else{
-                while(customers[i]){
-                    if(customers[i] <= 4){
-                        line.push_back(customers[i]);
-                    }else{
-                        line.push_back(4);
-                        customers[i] -= 4;
-                    }
-                }
-            }
-        }
+//         for(int i = 0; i < n; i++){
+//             if(i != (n - 1)){
+//                 if(customers[i] <= 4){
+//                     line.push_back(customers[i]);
+//                 }else{
+//                     int temp = customers[i] / 4;
+//                     for(int j = 0; j < temp; j++){
+//                         line.push_back(4);
+//                     }
+//                     customers[i + 1] += customers[i] % 4;
+//                 }
+//             }else{
+//                 while(customers[i]){
+//                     if(customers[i] <= 4){
+//                         line.push_back(customers[i]);
+//                     }else{
+//                         line.push_back(4);
+//                         customers[i] -= 4;
+//                     }
+//                 }
+//             }
+//         }
 
-        for(auto x : line){
-            cout << x << " ";
-        }
-    }
-};
+//         for(auto x : line){
+//             cout << x << " ";
+//         }
+//     }
+// };
 
+
+// int main(){
+//         vector<int> customers{10,9,6};
+//         int n = customers.size();
+//         vector<int> line;
+//         int ans = 0;
+//         // int wait = 0;
+//         // int lun = 0;
+
+//         for(int i = 0; i < n; i++){
+//             if(i != (n - 1)){
+//                 if(customers[i] <= 4){
+//                     line.push_back(customers[i]);
+//                 }else{
+//                     int temp = customers[i] / 4;
+//                     for(int j = 0; j < temp; j++){
+//                         line.push_back(4);
+//                     }
+//                     customers[i + 1] += customers[i] % 4;
+//                 }
+//             }else{
+//                 while(customers[i]){
+//                     if(customers[i] <= 4){
+//                         line.push_back(customers[i]);
+//                     }else{
+//                         line.push_back(4);
+//                         customers[i] -= 4;
+//                     }
+//                 }
+//             }
+//         }
+
+//         for(auto x : line){
+//             cout << x << " ";
+//         }
+//     return 0;
+// }
+
+#include<iostream>
+#include <ostream>
 
 int main(){
-        vector<int> customers{10,9,6};
-        int n = customers.size();
-        vector<int> line;
-        int ans = 0;
-        // int wait = 0;
-        // int lun = 0;
-
-        for(int i = 0; i < n; i++){
-            if(i != (n - 1)){
-                if(customers[i] <= 4){
-                    line.push_back(customers[i]);
-                }else{
-                    int temp = customers[i] / 4;
-                    for(int j = 0; j < temp; j++){
-                        line.push_back(4);
-                    }
-                    customers[i + 1] += customers[i] % 4;
-                }
-            }else{
-                while(customers[i]){
-                    if(customers[i] <= 4){
-                        line.push_back(customers[i]);
-                    }else{
-                        line.push_back(4);
-                        customers[i] -= 4;
-                    }
-                }
-            }
-        }
-
-        for(auto x : line){
-            cout << x << " ";
-        }
-    return 0;
+    int i = 10, j = 10;
+    std::cout << i++ << " " << ++j << std::endl;
+    std::cout << i << " " << j;
 }
