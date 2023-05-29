2548c43a2d0823f772bbffbd5f1b29fc0191502d
YuJiang <897862595@qq.com>
2023-05-29 11:07:05 +0800

    list series

--- cppPrime/2/pointer.cpp
+++ cppPrime/2/pointer.cpp
@@ -2,53 +2,24 @@
 using namespace std;
 
 int main(){
-    // int a = 10;
-    // int *p = &a;
-    // int **pp = &p;
-    // cout << **pp << "  " << *pp << "  " << *p << "  " << p;
-    // cout << endl;
+    int a = 1;
 
-    // //----------------------------------------
-    // int *&r = p;
-    // cout << r;
+    int *p2;
+    p2 = &a;
+    cout << p2 << endl;
+    cout << *p2 << endl;
 
-    // int a = 10;
-    // int* p = &a;
-    // cout << "*p: " << *p << endl;
-    // cout << "p: " << p << endl;
-    // *p = 0;
-    // cout << *p << '  ' << a;
-    // cout << *p << endl;
-    // cout << a << endl;
+    int **pp2 = &p2;
+    cout << pp2 << endl;
+    cout << *pp2 << endl;
+    cout << **pp2 << endl;
 
-    // int *ep1 = nullptr;
-    // int *ep2 = 0;
-    // int *ep3 = NULL;
-    // int a = 10;
+    int *&r = p2;
+    r = &a;
+    *r = 0;
+    
+    const int &c1 = 10;
+    cout << c1;
 
-    // cout << *ep1 << endl;
-    // cout << *ep2 << endl;
-    // cout << *ep3 << endl;
-
-    int i = 10;
-    int j = 5;
-
-    int *p1 = &i;
-    int *p2 = &j;
-    int *p3 = 0;
-
-    if(!p3){
-        cout << "*p1: " << *p1 << endl;
-    }
-
-    p3 = p2;
-
-    if(p3 == p2){
-        cout << "*p3: " << *p3 << endl;
-    }
-
-
-
-    return 0;
 
 }
