b967a40c1161be1144c535389123e6bfbb0a18d0
YuJiang <897862595@qq.com>
2023-06-08 20:40:05 +0800

    test reverse list

--- leetcode/92/92.cpp
+++ leetcode/92/92.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <ostream>
 //  * Definition for singly-linked list.
 struct ListNode {
     int val;
@@ -32,3 +33,32 @@ public:
 
     }
 };
+
+int main(){
+    ListNode* a1 = new ListNode{1};
+    ListNode* a2 = new ListNode{2};
+    ListNode* a3 = new ListNode{3};
+    ListNode* a4 = new ListNode{4};
+    ListNode* a5 = new ListNode{5};
+    a1->next = a2;
+    a2->next = a3;
+    a3->next = a4;
+    a4->next = a5;
+    // a5->next = nullptr;
+
+    ListNode* cur = a1;
+    while(cur){
+        std::cout << cur->val << " ";
+        cur = cur->next;
+    }
+    std::cout << std::endl;
+
+    Solution s1;
+    s1.reverseBetween(a1, 2, 4);
+
+    ListNode* curr = a1;
+    while(curr){
+        std::cout << curr->val << " ";
+        curr = curr->next;
+    }
+}
